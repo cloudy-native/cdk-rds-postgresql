097e44f2758e1d873f66d960ed99831e6a3b6b63
Stephen Harrison <stephen@stephen.cloud>
2021-12-29 16:56:30 -0500

    Add database

--- lib/cdk-rds-postgresql-stack.ts
+++ lib/cdk-rds-postgresql-stack.ts
@@ -1,19 +1,47 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
+import {
+  Peer,
+  Port,
+  SecurityGroup,
+  SubnetType,
+  Vpc,
+} from "aws-cdk-lib/aws-ec2";
+import {
+  AuroraPostgresEngineVersion,
+  DatabaseCluster,
+  DatabaseClusterEngine,
+} from "aws-cdk-lib/aws-rds";
+import { Construct } from "constructs";
 
 export class CdkRdsPostgresqlStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkRdsPostgresqlQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const vpc = new Vpc(this, "vpc", {
+      maxAzs: 2,
+      natGateways: 1,
     });
 
-    const topic = new sns.Topic(this, 'CdkRdsPostgresqlTopic');
+    const securityGroup = new SecurityGroup(this, "security-group", {
+      vpc,
+    });
+    securityGroup.addIngressRule(Peer.ipv4("100.0.58.54/32"), Port.tcp(5432));
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const database = new DatabaseCluster(this, "database", {
+      engine: DatabaseClusterEngine.auroraPostgres({
+        version: AuroraPostgresEngineVersion.VER_13_4,
+      }),
+      instanceProps: {
+        vpcSubnets: {
+          subnetType: SubnetType.PUBLIC,
+        },
+        vpc,
+        securityGroups: [securityGroup],
+      },
+    });
+
+    new CfnOutput(this, "endpoint", {
+      value: database.clusterEndpoint.hostname,
+    });
   }
 }
